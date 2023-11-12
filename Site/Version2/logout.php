f4fa7fd82d951fff1a9d0332c2d760924ffe7665
Cyril <126687961+BluenessDev@users.noreply.github.com>
2023-11-12 16:49:37 +0100

    Delete Site/Version2 directory
    mauvaise branche

--- Site/Version2/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_GET['out'])) {
-    unset($_SESSION['login'], $_SESSION['date']);
-    session_destroy();
-    header('Location: index.php');
-} else {
-    header('Location: index.php');
-}
