aaaa0c8da7016679090bbc9d0a0fdf61ce5c9cf9
Maaz Norat <maaznorat@gmail.com>
2024-01-05 20:24:25 +0100

    ajout de la clé étrangère technicien_login qui permettra d'assigner un technicien a un ticket par l'administrateur

--- Site/TempDB/tickets.sql
+++ Site/TempDB/tickets.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 22 déc. 2023 à 19:37
+-- Généré le : jeu. 04 jan. 2024 à 16:26
 -- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.0.30
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,22 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tickets` (
-  `id` int(11) NOT NULL,
-  `nature` varchar(100) NOT NULL,
-  `niveau` int(1) NOT NULL,
-  `salle` varchar(3) NOT NULL,
-  `demandeur` varchar(100) NOT NULL,
-  `concerne` varchar(100) NOT NULL,
-  `description` varchar(400) NOT NULL,
-  `etat` varchar(50) NOT NULL DEFAULT 'ouvert',
-  `login` varchar(15) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp()
+                           `id` int(11) NOT NULL,
+                           `nature` varchar(100) NOT NULL,
+                           `niveau` int(1) NOT NULL,
+                           `salle` varchar(3) NOT NULL,
+                           `demandeur` varchar(100) NOT NULL,
+                           `concerne` varchar(100) NOT NULL,
+                           `description` varchar(400) NOT NULL,
+                           `etat` varchar(50) NOT NULL DEFAULT 'ouvert',
+                           `login` varchar(15) NOT NULL,
+                           `date` date NOT NULL DEFAULT current_timestamp(),
+                           `technicien_login` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `tickets`
---
-
 
 --
 -- Index pour les tables déchargées
@@ -53,7 +49,8 @@ CREATE TABLE `tickets` (
 -- Index pour la table `tickets`
 --
 ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`),
+  ADD KEY `technicien_login` (`technicien_login`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -63,7 +60,17 @@ ALTER TABLE `tickets`
 -- AUTO_INCREMENT pour la table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `tickets`
+--
+ALTER TABLE `tickets`
+    ADD CONSTRAINT `fk_technicien_login` FOREIGN KEY (`technicien_login`) REFERENCES `users` (`login`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
